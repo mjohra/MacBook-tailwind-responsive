e02a1cf8279e6a607254aaca7a99ebc7bf1f6a73
mjohra <mehjabin.johra00@gmail.com>
2021-08-21 17:33:14 +0600

    updated

--- script.js
+++ script.js
@@ -15,40 +15,32 @@ const shippingTotal = document.getElementById("shipping");
 const price = document.getElementById("price");
 const promoCode = document.getElementById("code");
 
-free.addEventListener("click", function () {
-  memoryField.innerText = "0";
-  price.innerText = "1299";
+function update(cost, element) {
+  const productField = document.getElementById(element);
+  productField.innerText = cost;
   updateTotal();
+}
+
+free.addEventListener("click", function () {
+  update(0, "memory");
 });
 express.addEventListener("click", function () {
-  memoryField.innerText = "180";
-  price.innerText = "1299";
-  updateTotal();
+  update(180, "memory");
 });
 normalStorage.addEventListener("click", function () {
-  storageField.innerText = "0";
-  price.innerText = "1299";
-  updateTotal();
+  update(0, "storage");
 });
 mediumStorage.addEventListener("click", function () {
-  storageField.innerText = "100";
-  price.innerText = "1299";
-  updateTotal();
+  update(100, "storage");
 });
 expressStorage.addEventListener("click", function () {
-  storageField.innerText = "180";
-  price.innerText = "1299";
-  updateTotal();
+  update(180, "storage");
 });
 freeDelivery.addEventListener("click", function () {
-  deliveryField.innerText = "0";
-  price.innerText = "1299";
-  updateTotal();
+  update(0, "delivery");
 });
 paidDelivery.addEventListener("click", function () {
-  deliveryField.innerText = "20";
-  price.innerText = "1299";
-  updateTotal();
+  update(20, "delivery");
 });
 
 //update total
@@ -57,27 +49,26 @@ function updateTotal() {
   const storageCharge = Number(storageField.innerText);
   const deliveryCharge = Number(deliveryField.innerText);
   const bestPrice = Number(price.innerText);
+  const total = document.getElementById("total");
+  const shippingTotal = document.getElementById("shipping");
   const grandTotal = memoryCharge + storageCharge + deliveryCharge + bestPrice;
   total.innerText = grandTotal;
   shippingTotal.innerText = grandTotal;
 }
 
 //promo code
-const discountPrice = Number(shippingTotal.innerText);
+
 function applyPromoCode() {
+  const total = document.getElementById("total");
+  const discount = Number(total.innerText);
+  const shippingTotal = document.getElementById("shipping");
   const promoCode = document.getElementById("code").value;
   if (promoCode == "stevekaku") {
-    const discount=discountPrice;
-    const prevTotal = discount * 0.2;
-    //console.log(prevTotal);
-    const currentTotal = discountPrice - prevTotal;
-    //const total=currentTotal;
+    const currentTotal = discount - (discount * 0.2);
     document.getElementById("code").value = "";
     shippingTotal.innerText = currentTotal;
-    //currentTotal=Number(shippingTotal.innerText);
   } else {
     alert('enter valid coupon "stevekaku"');
     document.getElementById("code").value = "";
   }
 }
-
